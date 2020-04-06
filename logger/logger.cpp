851302716b191be6a9a69e55329a4e027b338896
Henry Wilson <henry.wilson.17@cnu.edu>
2020-04-05 22:22:06 -0400

    Reminding comments in logger.cpp

--- logger/logger.cpp
+++ logger/logger.cpp
@@ -5,10 +5,16 @@ Logger::Logger(std::string filename) {
 	this->filename = filename;
 }
 
+/*
+ * Ensure that all files are closed and all mutexes released
+ */
 Logger::~Logger() {
 }
 
 //open close and clear the log file
+/*
+ * This function is not completed. We need to actually clear the file.
+ */
 void Logger::clearlogfile() {
 	myFile.open(filename, std::fstream::trunc);
 
@@ -17,6 +23,9 @@ void Logger::clearlogfile() {
 		myFile.close();
 }
 
+/*
+ * This appears to be done.
+ */
 void Logger::log(std::string data) {
 	myFile.open(filename, std::fstream::app);
 	if (!myFile.is_open())
