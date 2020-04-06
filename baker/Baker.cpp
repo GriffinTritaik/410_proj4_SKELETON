19b8a9410ab7d11fa01ba5b8a1cbd4800d57dd46
Henry Wilson <henry.wilson.17@cnu.edu>
2020-04-05 22:20:12 -0400

    Comments in baker.cpp to remind us

--- baker/Baker.cpp
+++ baker/Baker.cpp
@@ -1,20 +1,37 @@
 #include <mutex>
 
+//Will need to include externs.h
 #include "../includes/baker.h"
 using namespace std;
 
+/*
+ * I believe that this ID will be provided by a thread spawning loop;
+ * we'll want 20 bakers on different threads with IDs 0-19
+ */
 Baker::Baker(int id):id(id)
 {
 }
 
+/*
+ * No fancy data structures, no actual implementatio.
+ * Is this true?
+ */
 Baker::~Baker()
 {
 }
 
+/*
+ * I assume we get an order object from the queue
+ */
 void Baker::bake_and_box(ORDER &anOrder) {
 	
 }
 
+/*
+ * The actual behavior of a baker that causes thread interactions.
+ * As long as there are orders in order_in_Q,
+ * 		behave appropriately
+ */
 void Baker::beBaker() {
 	
 }
