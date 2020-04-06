92f6d96b0973f511533929e47e7362f4ab9f65d4
haka <haka@ubuntu>
2020-04-05 19:38:47 -0700

    testing pushes

--- waiter/waiter.cpp
+++ waiter/waiter.cpp
@@ -3,8 +3,15 @@
 
 #include "../includes/waiter.h"
 
+/*
+ * this is where will will need to use extern.h's conditional variable to make sure we write before we bake!
+ */
+
 using namespace std;
 
+//ID is just a number used to identify this particular baker
+//(used with PRINT statements)
+//filename is what waiter reads in orders from
 Waiter::Waiter(int id,std::string filename):id(id),myIO(filename){
 }
 
@@ -12,11 +19,19 @@ Waiter::~Waiter()
 {
 }
 
-//gets next Order(s) from file_IO
+//gets next Order from file_IO
+//if return == SUCCESS then anOrder
+//contains new order
+//otherwise return contains fileIO error
 int Waiter::getNext(ORDER &anOrder){
 	return SUCCESS;
 }
 
+//contains a loop that will get orders from filename one at a time
+//then puts them in order_in_Q then signals baker(s) using cv_order_inQ
+//so they can be consumed by baker(s)
+//when finished exits loop and signals baker(s) using cv_order_inQ that
+//it is done using b_WaiterIsFinished
 void Waiter::beWaiter() {
 }
 
