f3772e49adcb7eb8ea240171c3c9b3b75792674f
Henry Wilson <henry.wilson.17@cnu.edu>
2020-04-08 17:18:37 -0400

    First pass done on logger.cpp. Assume correct; threasafe; destructed.

--- logger/logger.cpp
+++ logger/logger.cpp
@@ -1,5 +1,15 @@
+/*
+ * logger.cpp
+ *
+ * Henry R. Wilson and Griffin P. Tritaik
+ *
+ * 2020-04
+ */
+
 #include "../includes/logger.h"
-using namespace std;
+
+//using namespace std;
+//Henry Wilson hates this
 
 Logger::Logger(std::string filename) {
 	this->filename = filename;
@@ -9,33 +19,53 @@ Logger::Logger(std::string filename) {
  * Ensure that all files are closed and all mutexes released
  */
 Logger::~Logger() {
+	//Should never be true at this point, but just to check...
+	if(myFile.is_open()){
+		myFile.close();
+	}
+	//Not releasing m (mutex), as is ONLY used with std::lock_guard.
+	//Raw mutices are not safe.
 }
 
 //open close and clear the log file
 /*
- * This function is not completed. We need to actually clear the file.
+ * threadsafe due to explicitly scoped lock_guard
  */
 void Logger::clearlogfile() {
+	//makes section thread-safe
+    {
+    std::lock_guard<std::mutex> lock(m);
+
+	//Turns out this truncate flag does all the work for us
 	myFile.open(filename, std::fstream::trunc);
 
 	//close file
-	if (myFile.is_open())
+	if (myFile.is_open()){
 		myFile.close();
+	}
+    }
 }
 
 /*
- * This appears to be done.
+ * Threadsafe due to explicitly scoped lock_guard
  */
 void Logger::log(std::string data) {
+
+	{
+    std::lock_guard<std::mutex> lock(m);
+
 	myFile.open(filename, std::fstream::app);
-	if (!myFile.is_open())
+	if (!myFile.is_open()){
 		return;
+	}
 
 	std::string myline;
 
 	myFile << data;
 
 	//close file
-	if (myFile.is_open())
+	if (myFile.is_open()){
 		myFile.close();
+	}
+	}
 }
